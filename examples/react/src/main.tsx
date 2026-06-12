aad3a05965a3713ef84d68331d5549c6a292b122
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-06-12 21:28:21 +0800

    chore: update example

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './app.tsx'
 
-import './index.css'
+import './main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
