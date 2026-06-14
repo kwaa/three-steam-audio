adf12b307cf3b7cb756bfe224ea1fb0390d9b385
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-06-14 21:47:13 +0800

    docs: set shiki theme

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
             },
           }),
         ],
+        themes: ['one-light', 'github-dark-dimmed'],
       },
       plugins: [
         starlightThemeNova(),
