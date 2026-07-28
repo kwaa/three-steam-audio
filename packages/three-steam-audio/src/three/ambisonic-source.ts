f62ad2444570d1b682e5893cc962c1db8d3bef98
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-07-28 14:01:17 +0800

    fix: review

--- packages/three-steam-audio/src/three/ambisonic-source.ts
+++ packages/three-steam-audio/src/three/ambisonic-source.ts
@@ -7,23 +7,22 @@ import { Quaternion, Vector3 } from 'three'
 import { SteamAudioError } from './errors'
 import { normalizeQuaternion } from './settings'
 
-const sourceAhead = new Vector3()
-const sourceUp = new Vector3()
-const ahead = new Vector3()
-const up = new Vector3()
-const right = new Vector3()
-const listenerAhead = new Vector3()
-const listenerUp = new Vector3()
-const listenerRight = new Vector3()
-const listenerInverse = new Quaternion()
-
 /** Integration-layer ambisonic source corresponding to Unity's SteamAudioAmbisonicSource. */
 export class AmbisonicSourceImpl implements AmbisonicSource {
   readonly nodes = new Set<SteamAudioAmbisonicNode>()
 
+  readonly #ahead = new Vector3()
   #binaural: boolean
   #disposed = false
+  readonly #listenerAhead = new Vector3()
+  readonly #listenerInverse = new Quaternion()
+  readonly #listenerRight = new Vector3()
+  readonly #listenerUp = new Vector3()
   readonly #orientation = new Quaternion()
+  readonly #right = new Vector3()
+  readonly #sourceAhead = new Vector3()
+  readonly #sourceUp = new Vector3()
+  readonly #up = new Vector3()
   readonly #world: WorldImpl
 
   constructor(world: WorldImpl, settings?: AmbisonicSourceSettings) {
@@ -52,29 +51,29 @@ export class AmbisonicSourceImpl implements AmbisonicSource {
   publishControl(): void {
     this.assertActive('AmbisonicSource.publishControl')
     const listenerOrientation = this.#world.listenerImpl.orientation
-    listenerInverse.copy(listenerOrientation).invert()
-    sourceAhead.set(0, 0, -1).applyQuaternion(this.#orientation)
-    sourceUp.set(0, 1, 0).applyQuaternion(this.#orientation)
-    sourceAhead.applyQuaternion(listenerInverse)
-    sourceUp.applyQuaternion(listenerInverse)
+    this.#listenerInverse.copy(listenerOrientation).invert()
+    this.#sourceAhead.set(0, 0, -1).applyQuaternion(this.#orientation)
+    this.#sourceUp.set(0, 1, 0).applyQuaternion(this.#orientation)
+    this.#sourceAhead.applyQuaternion(this.#listenerInverse)
+    this.#sourceUp.applyQuaternion(this.#listenerInverse)
 
-    ahead.set(sourceAhead.x, sourceAhead.y, -sourceAhead.z).normalize()
-    up.set(sourceUp.x, sourceUp.y, -sourceUp.z).normalize()
-    right.crossVectors(ahead, up).normalize()
-    listenerAhead.set(-right.z, -up.z, ahead.z).normalize()
-    listenerUp.set(right.y, up.y, -ahead.y).normalize()
-    listenerRight.crossVectors(listenerAhead, listenerUp).normalize()
+    this.#ahead.set(this.#sourceAhead.x, this.#sourceAhead.y, -this.#sourceAhead.z).normalize()
+    this.#up.set(this.#sourceUp.x, this.#sourceUp.y, -this.#sourceUp.z).normalize()
+    this.#right.crossVectors(this.#ahead, this.#up).normalize()
+    this.#listenerAhead.set(-this.#right.z, -this.#up.z, this.#ahead.z).normalize()
+    this.#listenerUp.set(this.#right.y, this.#up.y, -this.#ahead.y).normalize()
+    this.#listenerRight.crossVectors(this.#listenerAhead, this.#listenerUp).normalize()
 
     const orientation: AmbisonicNodeControlValues['orientation'] = [
-      listenerAhead.x,
-      listenerAhead.y,
-      listenerAhead.z,
-      listenerUp.x,
-      listenerUp.y,
-      listenerUp.z,
-      listenerRight.x,
-      listenerRight.y,
-      listenerRight.z,
+      this.#listenerAhead.x,
+      this.#listenerAhead.y,
+      this.#listenerAhead.z,
+      this.#listenerUp.x,
+      this.#listenerUp.y,
+      this.#listenerUp.z,
+      this.#listenerRight.x,
+      this.#listenerRight.y,
+      this.#listenerRight.z,
     ]
     for (const node of this.nodes)
       node.setControl({ binaural: this.#binaural, orientation })
